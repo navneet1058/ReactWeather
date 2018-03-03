6327dea0cee7324421bfcaf9ccda9c4cdc3eaee7
navneet sharma <navneet1058@gmail.com>
2018-03-04 00:17:19 +0530

    Initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var About = React.createClass({
+    render: function(){
+        return (
+            <h3>About Component</h3>
+        );
+    }
+});
+
+module.exports = About;

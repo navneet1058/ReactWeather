9edd95c6091a3a72e1e6a0d56827a02063efe2e9
navneet sharma <navneet1058@gmail.com>
2018-03-04 14:07:45 +0530

    Add Foundation and jQuery

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-var express = require("express");
+var express = require('express');
+
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
-
-app.use(function(req, res, next){
-    if(req.headers['x-forwarded-proto'] === "http"){
-        next();  
-    } else {
-        res.redirect('http://'+ req.hostname + req.url);
-    } 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
 });
 
 app.use(express.static('public'));
-app.listen(port, function(){
-    console.log('Express server is up on port ' + port);
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
